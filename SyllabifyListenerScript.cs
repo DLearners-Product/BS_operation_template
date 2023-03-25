b43846797a6083d2f0c88884aa18d0acf02268f6
Jaikishore <jaikishore@dlearners.in>
2023-03-25 19:53:26 +0530

    implemented new script for syllabification

---
+++ SyllabifyListenerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SyllabifyListenerScript : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(
+            () => { 
+                BlendedOperations.instance.SendDataToSylabify((gameObject.GetComponent<Text>()) ? gameObject.GetComponent<Text>().text : gameObject.GetComponent<TMP_Text>().text);
+            }
+        );
+    }
+}
