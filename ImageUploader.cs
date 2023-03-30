399cd6707332d0b0dd6ee54363f2b0fbb78e883e
Jaikishore <jaikishore@dlearners.in>
2023-03-30 20:00:39 +0530

    image uploader implemented

---
+++ ImageUploader.cs
@@ -0,0 +1,10 @@
+﻿// using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// [FilePath("ProjectSettings/ImageUploader.asset", FilePathAttribute.Location.PreferencesFolder)]
+public class ImageUploader : ScriptableSingleton<ImageUploader>
+{
+    List<GameObject> activityGO;
+}
