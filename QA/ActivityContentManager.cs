f35bd9a9a01e1c0a8b45f33e88b47f297149ddf5
Jaikishore <jaikishore@dlearners.in>
2023-04-08 20:04:54 +0530

    changed folder strcuture

---
+++ QA/ActivityContentManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ActivityContentManager : MonoBehaviour
+{
+    [SerializeField] int questionCount;
+    [SerializeField] private QuestionType questionType;
+    public ActivityQAManager activityQAData;
+
+    private void OnValidate()
+    {
+        Debug.Log($"Came here");
+        UpdateInspector();
+    }
+
+    void UpdateInspector(){
+        switch(questionType){
+            case QuestionType.Static:
+                Debug.Log($"Question type is static "+(activityQAData.GetType() == typeof(StaticQA)));
+                Debug.Log($"Question type is static "+(activityQAData.GetType() == typeof(DynamicQA)));
+                Debug.Log($"Question type is static "+(activityQAData == null));
+
+                activityQAData = new StaticQA(questionCount);
+                break;
+            
+            case QuestionType.Dynamic:
+                Debug.Log($"Question type is dynamic "+(activityQAData.GetType() == typeof(DynamicQA)));
+                activityQAData = new DynamicQA(questionCount);
+                break;
+
+            default:
+                Debug.Log($"Question type is none");
+                activityQAData = null;
+                break;
+        }
+    }
+}
---
+++ QA/ActivityQAManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ActivityQAManager
+{
+    protected int questionCount;
+    public FileType questionType;
+    public FileType optionType;
+
+    public ActivityQAManager(int count){
+        this.questionCount = count;
+    }
+}
+
+[Serializable]
+public class QA{
+    public Component question;
+    public Component answer;
+}
+
+[Serializable]
+public class DynamicQA : ActivityQAManager{
+    public Component question;
+    public Component answer;
+    [SerializeReference] public Component[] option;
+
+    public DynamicQA(int count) : base(count){
+        option = new Component[questionCount];
+
+        for(int i=0; i<questionCount; i++){
+            option[i] = new Component();
+        }
+    }
+}
+
+[Serializable]
+public class StaticQA : ActivityQAManager{
+    public QA[] questions;
+    public Component[] options;
+    
+    public StaticQA(int count) : base(count){
+        questions = new QA[questionCount];
+        options = new Component[questionCount];
+
+        for(int i=0; i<questionCount; i++){
+            options[i] = new Component();
+        }
+    }
+}
