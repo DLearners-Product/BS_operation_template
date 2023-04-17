f176186e53fe94b0d62f4f8a653f9c37dcc1c1ac
Jaikishore <jaikishore@dlearners.in>
2023-04-17 10:32:17 +0530

    implemented get activiy text data

--- QA/ActivityQAManager.cs
+++ QA/ActivityQAManager.cs
@@ -23,12 +23,32 @@ public abstract class ActivityQA
 public class QA{
     public Component question;
     public Component[] answers;
+    string strData;
     
     public void Update(){
-        question.UpdateDimension();
+        question.UpdateAssets();
         foreach(var answer in answers){
-            answer.UpdateDimension();
+            answer.UpdateAssets();
         }
+    }
+
+    string GetArrayData(Component[] components){
+        string data = "";
+        for (int i=0; i < components.Length; i++)
+        {
+            data += components[i].GetComponentStringfyData();
+            if(i < (components.Length -1)){
+                data += ",";
+            }
+        }
+        return data;
+    }
+
+    public string GetData(){
+        strData = "{";
+        strData += $"\"question\":{question.GetComponentStringfyData()}, \"answer\":[";
+        strData += GetArrayData(answers) + "]}";
+        return strData;
     }
 }
 
@@ -37,22 +57,44 @@ public class QAO{
     public Component question;
     public Component[] answers;
     public Component[] options;
+    string strData;
 
     public void Update(){
-        question.UpdateDimension();
+        question.UpdateAssets();
         foreach(var answer in answers){
-            answer.UpdateDimension();
+            answer.UpdateAssets();
         }
 
         foreach(var option in options){
-            option.UpdateDimension();
+            option.UpdateAssets();
         }
+    }
+
+    string GetArrayData(Component[] components){
+        string data = "";
+        for (int i=0; i < components.Length; i++)
+        {
+            data += components[i].GetComponentStringfyData();
+            if(i < (components.Length -1)){
+                data += ",";
+            }
+        }
+        return data;
+    }
+
+    public string GetData(){
+        strData = "{";
+        strData += $"\"question\":{question.GetComponentStringfyData()}, \"answer\":[";
+        strData += GetArrayData(answers) + "], \"option\":[";
+        strData += GetArrayData(options) + "]}";
+        return strData;
     }
 }
 
 [Serializable]
 public class DynamicQA : ActivityQA{
     public QAO[] questions;
+    string qaData;
 
     public DynamicQA() : base(){}
 
@@ -81,12 +123,34 @@ public class DynamicQA : ActivityQA{
             UpdateAsset();
         }
     }
+
+    string GetArrayData(QAO[] qas){
+        string data = "";
+        for (int i=0; i < qas.Length; i++)
+        {
+            data += qas[i].GetData();
+            if(i < (qas.Length -1)){
+                data += ",";
+            }
+        }
+        return data;
+    }
+
+    public string GetQAData(){
+        qaData = "[";
+        for(int i=0; i<questions.Length; i++){
+            qaData += questions[i].GetData();
+        }
+        qaData += "]";
+        return qaData;
+    }
 }
 
 [Serializable]
 public class StaticQA : ActivityQA{
     public QA[] questions;
     public Component[] options;
+    string qaData;
     
     public StaticQA() : base(){}
 
@@ -108,7 +172,7 @@ public class StaticQA : ActivityQA{
             question.Update();
         }
         foreach(var option in options){
-            option.UpdateDimension();
+            option.UpdateAssets();
         }
     }
 
@@ -119,6 +183,24 @@ public class StaticQA : ActivityQA{
             UpdateAsset();
         }
     }
+
+    public string GetQAData(){
+        qaData = "[";
+        for(int i=0; i<questions.Length; i++){
+            qaData += questions[i].GetData();
+        }
+        qaData += "]";
+        return qaData;
+    }
+
+    public string GetOptionData(){
+        qaData = "[";
+        for(int i=0; i<options.Length; i++){
+            qaData += options[i].GetComponentStringfyData();
+        }
+        qaData += "]";
+        return qaData;
+    }
 }
 
 [Serializable]
@@ -127,6 +209,7 @@ public class ActivityContent{
     public int slideNo;
     public StaticQA staticQA = new StaticQA();
     public DynamicQA dynamicQA = new DynamicQA();
+    string activityData;
 
     public void UpdateAsset(){
 #if UNITY_EDITOR
@@ -139,5 +222,21 @@ public class ActivityContent{
                 break;
         }
 #endif
+    }
+
+    public string GetActivityData(){
+        activityData = "{";
+        activityData += $"\"Question Type\":{questionType.ToString()}";
+        activityData += $"\"Slide No.\":{slideNo.ToString()}";
+        switch(questionType){
+            case QuestionType.Static:
+                activityData += $"\"QA\":"+staticQA.GetQAData();
+                activityData += $"\"Option\":"+staticQA.GetOptionData();
+                break;
+            case QuestionType.Dynamic:
+                activityData += $"\"QA\":"+dynamicQA.GetQAData();
+                break;
+        }
+        return activityData;
     }
 }
